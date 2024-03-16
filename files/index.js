4386cc1e9492e2988975a58d5f57e58cf3fdde82
yizhipian <123932855+yizhipian@users.noreply.github.com>
2024-03-16 09:23:53 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -3,11 +3,11 @@ const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
 const UUID = process.env.UUID || '986e0d08-b275-4dd3-9e75-f3094b36fa2a'; //若需要改UUID，需要在config.json里改为一致
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
-const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'ct8.yizhipian.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZmI1MDA3Njg4NmNkZGRhODU3MjZiYzg4MTkxZjBiZTMiLCJ0IjoiYWY4NWNmMmEtMjE0Yi00ZWE5LWJkNDktY2MyZWYyMzI5YmJkIiwicyI6IllqRmpNekJsTXpndE5XRTBZeTAwTkRaa0xXSXlaRE10T1RSbU9EWmhNRFZpTWpNeCJ9';
+const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.ziliaoku.eu.org';     
+const NEZHA_PORT = process.env.NEZHA_PORT || '443';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
+const NEZHA_KEY = process.env.NEZHA_KEY || 'AbcGps7grVVnCCXLLO';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'cho.yizhipian.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZmI1MDA3Njg4NmNkZGRhODU3MjZiYzg4MTkxZjBiZTMiLCJ0IjoiZDE1ODEzYjItZWE1OS00ZmYwLTgxYTgtN2MzZmRlOWMzNTBiIiwicyI6Ik5tUXpOemcxTURndE9UazJNUzAwWVRFM0xXRXpOV0l0TlRkaE1XTXdOVFUxTjJaaiJ9';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
